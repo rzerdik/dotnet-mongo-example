5843da14de0988d4901844298e1ff4d59416d770
Filip Dorian Pindej <fpindej@users.noreply.github.com>
2022-02-21 01:06:12 +0100

    Create car model

---
+++ src/MongoExample.API/Models/Car.cs
@@ -0,0 +1,23 @@
+using System.ComponentModel.DataAnnotations;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace MongoExample.API.Models;
+
+public class Car
+{
+    [BsonId]
+    [BsonRepresentation(BsonType.ObjectId)]
+    public string Id { get; set; }
+
+    [Required]
+    public int VIN { get; set; }
+
+    [Required]
+    public string Brand { get; set; } = default!;
+
+    [Required]
+    public string Model { get; set; } = default!;
+    
+    public DateTime DateManufactured { get; set; }
+}
